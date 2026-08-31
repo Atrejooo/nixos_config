561710aaf1d47d68ce9b7b773fb120b179dc709d
aki <you@example.com>
2026-08-31 22:55:43 +0200

    ~ updated vamp

--- hosts/pc0/default.nix
+++ hosts/pc0/default.nix
@@ -9,7 +9,7 @@
     specialArgs = { inherit shared; };
     modules = with self.nixosModules; [
       {
-        config.style.theme = "vamp";
+        config.style.theme = "pale";
         config.style.keyboard = "qwerty";
         config.new-desktop.niri.extra = /* kdl */ ''
           output "DP-3" {
--- themes/vamp.nix
+++ themes/vamp.nix
@@ -1,37 +1,37 @@
 rec {
   wallpaper = ./wallpapers/vamp.png;
 
-  darkBase = "010004";
-  darkMain = "111112";
-  lightBase = "47071a";
-  lightMain = "7b0822";
-  lightHighlight = "9d0d1f";
+  darkBase = "010004"; # "#010004"
+  darkMain = "111112"; # "#111112"
+  lightBase = "47071a"; # "#47071a"
+  lightMain = "7b0822"; # "#7b0822"
+  lightHighlight = "9d0d1f"; # "#9d0d2f"
 
-  textMain = "906a8b";
-  textEmph0 = "ad0d2f";
-  textEmph1 = "dF8775";
+  textMain = "906a8b"; # "#906a8b"
+  textEmph0 = "ad0d2f"; # "#ad0d2f"
+  textEmph1 = textBlue; # "#dF8775"
   textEmph2 = textOrange;
-  textPale = "708a9b";
-  textSelect = "00236d";
+  textPale = "606a8b"; # "#606a8b"
+  textSelect = "00236d"; # "#00236d"
 
-  textDark = "111111";
-  textWhite = "cfdfff";
+  textDark = "111111"; # "#111111"
+  textWhite = "cfdfff"; # "#cfdfff"
 
-  textRed = "a60e22";
-  textOrange = "eF5350";
-  textYellow = "dF8775";
-  textGreen = "a3bd36";
-  textCyan = "91b1dC";
-  textBlue = "8287FC";
-  textMagenta = "a351FC";
+  textRed = "a60e22"; # "#b61e32"
+  textOrange = "eF5350"; # "#eF5350"
+  textYellow = "dF8775"; # "#dF8775"
+  textGreen = "a3bd36"; # "#a3bd36"
+  textCyan = "91b1dC"; # "#71b1bC"
+  textBlue = "8287FC"; # "#5287FC"
+  textMagenta = "a351fc"; # "#a351fc"
 
-  lspKeyword = "761e40";
+  lspKeyword = "861e40"; # "#861e40"
   lspBuiltin = textMagenta;
-  lspType = textRed;
-  lspFunction = "662ea0";
+  lspType = "ad0d4f"; # "#ad0d4f"
+  lspFunction = "906aeb"; # "#906aeb"
   lspMacro = textYellow;
   lspVariable = textMain;
-  lspProperty = textBlue;
+  lspProperty = textMain; # "#906abb"
   lspString = textCyan;
   lspPrimitive = textWhite;
   lspException = textRed;
